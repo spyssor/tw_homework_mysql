a34f89dbcc3303ffbf938dbca03fd14bd1ae8579
huyf <spyssor@gmail.com>
2019-05-26 22:42:46 +0800

    修改标点符号

--- practice1.sql
+++ practice1.sql
@@ -1,10 +1,10 @@
 # 1.创建数据库employee_db
 
-# 2.在数据库employee_db中创建table：`Employee`
+# 2.在数据库employee_db中创建table: `Employee`
 
 # 3.将`employee-data.csv`文件中的数据导入数据表Employee中
 
-# 4.在数据库employee_db中创建table：`Company`
+# 4.在数据库employee_db中创建table: `Company`
 
 # 5.将`company-data.csv`文件中的数据导入Company数据表中
 
--- practice3.sql
+++ practice3.sql
@@ -1,4 +1,4 @@
 #取得平均薪水最高的公司
-#输出公司名称和平均薪水：companyName avgSalary
+#输出公司名称和平均薪水: companyName avgSalary
 
 
